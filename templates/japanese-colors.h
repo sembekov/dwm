7ded25114fbfb04d330743eb4e079e67917b2007
sembekov <76952248+sembekov@users.noreply.github.com>
2024-02-10 13:24:54 +0600

    Initial commit

---
+++ templates/japanese-colors.h
@@ -0,0 +1,39 @@
+static char normfgcolor[] = "#bfcacc";
+static char normbgcolor[] = "#020D15";
+static char normbordercolor[] = "#4b4f4f";
+static char normfloatcolor[] = "#4b4f4f";
+
+static char selfgcolor[] = "#bfcacc";
+static char selbgcolor[] = "#33585c";
+static char selbordercolor[] = "#33585c";
+static char selfloatcolor[] = "#33585c";
+
+static char titlenormfgcolor[] = "#bfcacc";
+static char titlenormbgcolor[] = "#020D15";
+static char titlenormbordercolor[] = "#4b4f4f";
+static char titlenormfloatcolor[] = "#4b4f4f";
+
+static char titleselfgcolor[] = "#bfcacc";
+static char titleselbgcolor[] = "#33585c";
+static char titleselbordercolor[] = "#bfcacc";
+static char titleselfloatcolor[] = "#4b4f4f";
+
+static char tagsnormfgcolor[] = "#bfcacc";
+static char tagsnormbgcolor[] = "#020D15";
+static char tagsnormbordercolor[] = "#4b4f4f";
+static char tagsnormfloatcolor[] = "#4b4f4f";
+
+static char tagsselfgcolor[] = "#bfcacc";
+static char tagsselbgcolor[] = "#33585c";
+static char tagsselbordercolor[] = "#bfcacc";
+static char tagsselfloatcolor[] = "#4b4f4f";
+
+static char hidnormfgcolor[] = "#bfcacc";
+static char hidselfgcolor[] = "#576F78";
+static char hidnormbgcolor[] = "#020D15";
+static char hidselbgcolor[] = "#020D15";
+
+static char urgfgcolor[] = "#bfcacc";
+static char urgbgcolor[] = "#576F78";
+static char urgbordercolor[] = "#576F78";
+static char urgfloatcolor[] = "#4b4f4f";
