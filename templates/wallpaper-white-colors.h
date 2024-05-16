c33db3acce8b90e84a6c033cb02092d2d689d685
sembekov <76952248+sembekov@users.noreply.github.com>
2024-05-16 23:39:41 +0500

    Add new colorscheme

---
+++ templates/wallpaper-white-colors.h
@@ -0,0 +1,39 @@
+static char normfgcolor[] = "#eae4e3";
+static char normbgcolor[] = "#0a0b0a";
+static char normbordercolor[] = "#6b6766";
+static char normfloatcolor[] = "#6b6766";
+
+static char selfgcolor[] = "#eae4e3";
+static char selbgcolor[] = "#eae4e3";
+static char selbordercolor[] = "#eae4e3";
+static char selfloatcolor[] = "#eae4e3";
+
+static char titlenormfgcolor[] = "#eae4e3";
+static char titlenormbgcolor[] = "#0a0b0a";
+static char titlenormbordercolor[] = "#6b6766";
+static char titlenormfloatcolor[] = "#6b6766";
+
+static char titleselfgcolor[] = "#0a0b0a";
+static char titleselbgcolor[] = "#eae4e3";
+static char titleselbordercolor[] = "#eae4e3";
+static char titleselfloatcolor[] = "#6b6766";
+
+static char tagsnormfgcolor[] = "#eae4e3";
+static char tagsnormbgcolor[] = "#0a0b0a";
+static char tagsnormbordercolor[] = "#6b6766";
+static char tagsnormfloatcolor[] = "#6b6766";
+
+static char tagsselfgcolor[] = "#0a0b0a";
+static char tagsselbgcolor[] = "#eae4e3";
+static char tagsselbordercolor[] = "#eae4e3";
+static char tagsselfloatcolor[] = "#6b6766";
+
+static char hidnormfgcolor[] = "#eae4e3";
+static char hidselfgcolor[] = "#9C9274";
+static char hidnormbgcolor[] = "#0a0b0a";
+static char hidselbgcolor[] = "#0a0b0a";
+
+static char urgfgcolor[] = "#eae4e3";
+static char urgbgcolor[] = "#9C9274";
+static char urgbordercolor[] = "#9C9274";
+static char urgfloatcolor[] = "#6b6766";
